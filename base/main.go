8ae1193eeb01769d8eb9d2f4b07783557f886eb5
Eduardo Nohr Lessa <eduardonohr@gmail.com>
2023-12-05 22:49:37 -0300

    Improve base code

--- base/main.go
+++ base/main.go
@@ -1,17 +1,24 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"os"
 )
 
 func main() {
-	file, _ := os.ReadFile("input.txt")
-	input := string(file)
-	log.Println(part1(input))
-	log.Println(part2(input))
+	file, _ := os.ReadFile("input1.txt")
+	input1 := string(file)
+  fmt.Println("Part1:", part1(input1))
+
+  file, _ = os.ReadFile("input2.txt")
+  input2 := string(file)
+  fmt.Println("Part2:", part2(input2))
 }
 
-func part1(input string) int { return 1 }
+func part1(input string) int {
+  return 1
+}
 
-func part2(input string) int { return 1 }
+func part2(input string) int {
+  return 1
+}
