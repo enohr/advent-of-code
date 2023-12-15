b871827c741fa23999b61f40e27c223fa0cb5b05
Eduardo Nohr Lessa <eduardonohr@gmail.com>
2023-12-15 00:33:18 -0300

    Day 6 part1

---
+++ 2023/day-6/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+  file, _ := os.ReadFile("input1.txt")
+  input1 := strings.TrimRight(string(file), "\n")
+  fmt.Println("Part1:", part1(input1))
+
+  file, _ = os.ReadFile("input2.txt")
+  input2 := strings.TrimRight(string(file), "\n")
+  fmt.Println("Part2:", part2(input2))
+}
+
+func part1(input string) int {
+  parts := strings.Split(input, "\n")
+  times := strings.Fields(parts[0])[1:] 
+  distances := strings.Fields(parts[1])[1:]
+  result := 1
+  
+  for i, time := range times {
+    sum := 0
+    record, _ := strconv.Atoi(distances[i])
+    t, _ := strconv.Atoi(time)
+
+    for i := 0; i <= t; i++ {
+      ms := i
+      ml := t - i
+      traveled := ms * ml
+      if traveled > record {
+        sum++
+      }
+    }
+    result *= sum
+  }
+  return result
+}
+
+func part2(input string) int {
+  return 1
+}
---
+++ 2023/day-6/main_test.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"os"
+	"testing"
+  "strings"
+)
+
+type test struct {
+	name     string
+	given    string
+	expected int
+}
+
+func Test_Part1(t *testing.T) {
+  file, _ := os.ReadFile("input1.txt")
+  input := strings.TrimRight(string(file), "\n")
+  tests := []test{
+		{
+			name:     "Part 1",
+			given:    input,
+			expected: 288,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.given, func(t *testing.T) {
+			if got := part1(tt.given); got != tt.expected {
+				t.Errorf("got %d expected %d", got, tt.expected)
+			}
+		})
+	}
+}
+
+func Test_Part2(t *testing.T) {
+  file, _ := os.ReadFile("input2.txt")
+  input := strings.TrimRight(string(file), "\n")
+  tests := []test{
+		{
+			name:     "Part 2",
+			given:    input,
+			expected: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.given, func(t *testing.T) {
+			if got := part2(tt.given); got != tt.expected {
+				t.Errorf("got %d expected %d", got, tt.expected)
+			}
+		})
+	}
+}
